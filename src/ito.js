4492baff199e0ccf59adfe846e97da729a78e860
Tomoyuki SHIMIZU <Tomoyuki@Bourgogne.local>
2016-12-28 17:44:20 +0900

    minor update

--- src/ito.js
+++ src/ito.js
@@ -3,7 +3,7 @@
  * 
  * Copyright (c) 2017 KDDI Research, Inc.
  * Use of this source code is governed by Apache License, Version 2.0,
- * that can be found in LICENSE.md file.
+ * that can be found in LICENSE file.
  */
 
 'use strict';
